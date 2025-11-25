519f93582b76be16bdac8db226706176f6a35d6b
YunHae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-11-25 15:51:08 +0900

    Title: 부분문자열 위치 구하기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251125/부분문자열 위치 구하기/find-location-of-substring.js
+++ 251125/부분문자열 위치 구하기/find-location-of-substring.js
@@ -4,8 +4,10 @@ const input = fs.readFileSync(0).toString().trim().split("\n")
 const [inputStr, goalStr] = input
 
 if(inputStr.includes(goalStr)) {
-    let replaced = inputStr.replace(goalStr, "O")
-    console.log(replaced.indexOf("O"))
+    console.log(inputStr.indexOf(goalStr))
+    // let replaced = inputStr.replace(goalStr, "O")
+    // console.log(replaced)
+    // console.log(replaced.indexOf("O"))
 } else {
     console.log(-1)
 }
