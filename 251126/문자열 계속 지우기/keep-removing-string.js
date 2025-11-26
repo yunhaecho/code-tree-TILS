f965de877c1b30a5d7d842a442c733068d387d4a
YunHae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-11-26 18:20:47 +0900

    Title: 문자열 계속 지우기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251126/문자열 계속 지우기/keep-removing-string.js
+++ 251126/문자열 계속 지우기/keep-removing-string.js
@@ -1,19 +1,27 @@
 const fs = require("fs")
 const input = fs.readFileSync(0).toString().trim().split("\n")
+let a = input[0]
+const part = input[1]
 
-const [ a , part ] = input
-let removed = ""
-let len = a.length;
-for(let i = 0; i< a.len; i++) {
-    if(a[i] + a[i+1] === part) {
-        removed = a.slice(i,i+2);
-        len = removed.length
+let len = part.length
+let i = 0
+
+while(true){ //a = dsjhjhjkl part = hj
+    // i = 3 len = 2
+    if(a.includes(part)) {
+        let compare = a.slice(i,i+len) //compare = 3,5 => ds
+
+        if( compare === part) { // i3 hj === hj / i3 hj === hj / 
+            a = a.slice(0,i) + a.slice(i+len) //a = 03 + 5 = dsjkl
+            i = 0 
+        } else {
+            i++
+        }
+
+    } else {
+        console.log(a)
+        break
     }
-    console.log(removed)
-
 }
-
-console.log(removed)
-
 
 
