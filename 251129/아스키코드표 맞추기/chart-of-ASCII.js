859283ca42a18be170797b1b7078b423d45f1102
YunHae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-11-29 00:53:07 +0900

    Title: 아스키코드표 맞추기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251129/아스키코드표 맞추기/chart-of-ASCII.js
+++ 251129/아스키코드표 맞추기/chart-of-ASCII.js
@@ -3,8 +3,7 @@ const nums = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 
 let result = ''
 for(let num of nums) {
-    result += String.fromCharCode(num) + " 
-    "
+    result += String.fromCharCode(num) + " "
 }
 
 console.log(result)
