2adcabb70c9a6f1fbd58686df551faa830edcf4c
YunHae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-11-29 00:52:51 +0900

    Title: 아스키코드표 맞추기, Time: 77ms, Memory: 11MB, Status: Runtime Error - Codetree

---
+++ 251129/아스키코드표 맞추기/chart-of-ASCII.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+const nums = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let result = ''
+for(let num of nums) {
+    result += String.fromCharCode(num) + " 
+    "
+}
+
+console.log(result)
