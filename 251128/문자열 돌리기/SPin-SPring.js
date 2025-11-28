6c76f4ec0b6d7849af3e0897eba2551ebd7d4d14
YunHae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-11-28 16:44:21 +0900

    Title: 문자열 돌리기, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251128/문자열 돌리기/SPin-SPring.js
@@ -0,0 +1,8 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().trim()
+const len = input.length
+console.log(input)
+for(let i = 0; i < len; i++) {
+    input = input.slice(-1) + input.slice(0,4)
+    console.log(input)
+}
