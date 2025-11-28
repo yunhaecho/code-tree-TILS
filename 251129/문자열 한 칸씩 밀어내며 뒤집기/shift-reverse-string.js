80ac2e3bfee92dbe67b99ddf2c5428ffc61fe499
YunHae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-11-29 00:41:31 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 66ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251129/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+const input = fs.readFileSync(0).toString().trim().split("\n")
+
+const info = input[0].split(" ")
+let str = info[0]
+const order = input.slice(1).map(Number)
+
+for(let num of order) {
+    if(num === 1) {
+        str = str.slice(1) + str.slice(0 , 1)
+        console.log(str)
+    } 
+    
+    else if(num === 2) {
+        str = str.slice(-1)+ str.slice(0 , -1)
+        console.log(str)
+
+    } else if(num === 3 ){  
+        str = str.split("").reverse()
+        console.log(str.join(""))
+    }
+}
