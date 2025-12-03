448d4eb5bf34362d23963ee1d8d6a77a40e5bda1
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-03 19:52:37 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251203/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
+++ 251203/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
@@ -2,14 +2,20 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
 let [n1, n2] = input[0].split(" ").map(Number);
-let a = input[1].split(" ").map(Number);
-let b = input[2].split(" ").map(Number);
+let a = input[1].split(" ")
+let b = input[2].split(" ")
 
 // Please Write your code here.
 //b가 a에 연속부분수열인지 
 function solution (a,b) {
-    if(!a.join("").includes(b.join(""))) return "No"
-    return "Yes"
+
+    const len = b.length
+    for(let i = 0; i<= a.length - len; i++) {
+        let compare = a[i] + a[i+1] + a[i+2]
+        if(compare === b.join("")) return "Yes"
+    }
+    return "No"
+    
 }
 
 console.log(solution(a,b))
