3c263ce8268e8e5e5d968a987679f9b7f51d0d83
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-07 16:12:01 +0900

    Title: 특정 구간의 합, Time: 67ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251207/특정 구간의 합/sum-of-specific-intervals.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, m] = input[0].split(" ").map(Number);
+const A = input[1].split(" ").map(Number);
+const queries = input.slice(2).map(line => line.split(" ").map(Number));
+
+// Please write your code here.
+
+function solution(n , queries) {
+
+    for (let i = 0; i < A.length; i++) {
+        let sum = 0
+        let query = queries[i]
+        const [a, b] = query
+        queries.slice(a - 1, b).map(num => sum += num)
+        console.log(sum)
+    }
+}
+
+solution( n , queries)
