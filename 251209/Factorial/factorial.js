c46a8a879e21066e91fa9cc56ad1a896ffa08746
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-09 15:14:22 +0900

    Title: Factorial, Time: 60ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251209/Factorial/factorial.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const N = Number(input[0]);
+
+// Please write your code here.
+
+function factorial (num) {
+    if(num === 1) return 1
+
+    return factorial(num-1) * num
+}
+
+console.log(factorial(NH))
