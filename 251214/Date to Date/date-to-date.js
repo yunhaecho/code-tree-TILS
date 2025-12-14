79f8ee727a3565237e498980ccbdecad45515bcb
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-14 22:30:12 +0900

    Title: Date to Date, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251214/Date to Date/date-to-date.js
+++ 251214/Date to Date/date-to-date.js
@@ -7,12 +7,9 @@ let [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 
 function countDate() {
     const d = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-    const month1 = d.slice(1,m1).reduce((datesum,date) => datesum += date, 1)
-    const month2 = d.slice(1,m2).reduce((datesum,date) => datesum += date, 1)
-    
-    // console.log(month1,d1,month2,d2)
-    // console.log(month1+d1+1)
-    // console.log(month2+d2)
+    const month1 = d.slice(1,m1).reduce((datesum,date) => datesum += date, 0)
+    const month2 = d.slice(1,m2).reduce((datesum,date) => datesum += date, 0)
+
     return Math.abs((month2 + d2) - (month1 + d1) + 1)
 }
 console.log(countDate())
