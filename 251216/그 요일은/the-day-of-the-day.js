5575fc479f594dbeef5eed46f840a462ffbb9d9c
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-16 20:05:47 +0900

    Title: 그 요일은, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251216/그 요일은/the-day-of-the-day.js
+++ 251216/그 요일은/the-day-of-the-day.js
@@ -5,10 +5,11 @@ const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 const day = input[1]; //"Mon"
 
 // Please Write your code here.
-const dayInfo = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-const dateInfo = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 function countDay(m, d) {
+    const dayInfo = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    const dateInfo = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
     let startM = m // 2
     let startD = d // 5
     let cnt = 0
