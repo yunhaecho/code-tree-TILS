c6a62be520aee9f6c107bf6aa7fb920ad9fdbfed
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-16 19:52:38 +0900

    Title: 그 요일은, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251216/그 요일은/the-day-of-the-day.js
+++ 251216/그 요일은/the-day-of-the-day.js
@@ -6,38 +6,37 @@ const day = input[1]; //"Mon"
 
 // Please Write your code here.
 const dayInfo = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-const dateInfo = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+const dateInfo = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-function countDay(m,d) {
+function countDay(m, d) {
     let startM = m // 2
     let startD = d // 5
+    let cnt = 0
+    let i = 0 //시작은 월요일
 
-    let cnt = 1
-    let i = dayInfo.indexOf(day) + 1 
- 
-    while(true) {
-        if(startM === m2 && startD === d2) break // 3 9
-        if(i === 7 ) i = 0 // i = 1
+    while (true) {
+        i++ 
+        if (i === dayInfo.indexOf(day)) cnt++ //  4 === 4 / cnt = 5
         
-        if(startD === dateInfo[startM]) { // 7 === 29
-            if(startM === 12 ) {
-                 startM = 1
+        if (startM === m2 && startD === d2) break
+
+        if (i === 6) i = -1 
+
+        if (startD === dateInfo[startM]) { // 
+            if (startM === 12) {
+                startM = 1
             } else {
-                startM++ //
+                startM++
             }
             startD = 1
         }
-        // i = 0
-        if( i === dayInfo.indexOf(day)) cnt++ // 0 === 5 / cnt = 0
-        startD++ //8
-        i++ //i = 1
-        }
-
-        return cnt
+        startD++
     }
+    return cnt
+}
 
 
-console.log(countDay(m1,d1))
+console.log(countDay(m1, d1))
 
 
 
