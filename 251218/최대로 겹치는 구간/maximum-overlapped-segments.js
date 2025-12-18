f3f76bf0976f1fbd0b393708bafe1ea074074076
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-18 22:46:25 +0900

    Title: 최대로 겹치는 구간, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 251218/최대로 겹치는 구간/maximum-overlapped-segments.js
+++ 251218/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -4,30 +4,23 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const segments = input.slice(1).map((arr) =>  arr.split(" ").map(Number))
-
 // Please write your code here.
 function solution() {
     const arrLen = 200
-    let arr = Array(arrLen).fill(0)
-    let isNagative = false
+    const offSet = 100
+    let arr = Array(arrLen+1).fill(0)
 
     for(let i = 0; i < segments.length; i++) {
         const interval = segments[i]
-        let [s,e] = interval
+        let [s, e] = interval
 
-        if(s < 0 || e < 0) isNagative = true
-
-        if(isNagative) {
-            s += arrLen
-            e += arrLen
-        }
+        s += offSet
+        e += offSet              
 
         for(let j = s; j < e; j++) {
             arr[j]++
         }
     }
-
     return Math.max(...arr)
-
 }
 console.log(solution())
