ec6a441fd33937d4c8d5c27d9960196b814c5b8a
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-20 16:57:14 +0900

    Title: 흰검 칠하기, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251220/흰검 칠하기/painting-white-black.js
+++ 251220/흰검 칠하기/painting-white-black.js
@@ -2,43 +2,65 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-// const commands = input.slice(1).map(line => line.split(' '));
+const commands = input.slice(1).map(line => line.split(' '));
 
 // Please Write your code here.
 
 function solution() {
-    let arr = Array(20+1).fill(0) //200000
-    let s = 10 //100000
-
+    let tile = Array(20 + 1).fill(0) //200000
+    let W = Array(20 + 1).fill(0) //200000
+    let B = Array(20 + 1).fill(0) //200000
+    let s  = 10 //100000
     let b = 0
     let g = 0
     let w = 0
-
-    for(let i = 0; i < commands.length; i++) {
+    let e;
+    for (let i = 0; i < commands.length; i++) {
         const order = commands[i]
-        const e = Number(order[0])
+        let step = Number(order[0])
         const dir = order[1]
         
-        for(let j = s; j <= e; j++) {
-            if(arr[i] === 0 && w >= 2 || arr[i] === 0 && b >= 2 ) {
-                i = "X"
+        if (dir === "R") {
+            e = s + step
+        } 
+
+        if (dir === "L") {
+            e = s
+            s = e - step
+         }
+
+        for (let j = s; j < e; j++) {
+
+            if (tile[i] === "X") continue
+
+            if (dir === "R") {
+                tile[i] = "B"
+                B[i]++
+            }
+
+            if (dir === "L") {
+                 tile[i] = "W" 
+                 W[i]++
+            }
+
+            if (tile[i] != "X" && B[i] >= 2 && W[i] >= 2 ) {
+                tile[i] = "X" 
                 g++
-            }
-
-            if(arr[i] === "X") continue
-
-            if(dir === "R") {
-                b++
-                arr[i]++
-            }
-
-            if(dir === "L") {
-                w++
-                arr[i]--
             }
         }
     }
-    return arr
+
+    console.log(tile)
+    
+    tile.forEach((color) => {
+        if(color === "B") {
+            b++
+        } else if (color === "W") {
+            w++
+        }
+    })
+
+    console.log(w, b, g) 
 }
 
-console.log(solution())
+solution()
