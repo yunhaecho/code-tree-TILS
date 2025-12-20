554f6385b4f402be29d06da812647ab76d834567
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-20 14:46:20 +0900

    Title: 왔다 갔던 구역 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251220/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 251220/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -2,34 +2,37 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const commands = input.slice(1, n + 1);
+const commands = input.slice(1, n + 1)
 
 // Please Write your code here.
 
 function solution () {
     let arr = Array(40 + 1).fill(0)
+    let interval = []
     let s = 20
-
     for(let i = 0; i < commands.length; i++) {
-        const order = commands[i] 
-        let e = Number(order[0])
+        const order = commands[i].split(" ")
+        let e = Number(order[0]) + s
         const d = order[1]
 
-        console.log(d)
-
         if(d === "R") {
-            e += s // e = 2 + 20 = 22
-            console.log(s,e)
-            arr[s]++
-            arr[e]++
-        } else {
-            e = s - e
-            arr[s]++
-            arr[e]++
+            e += s
+            interval.push([s,e])
         }
-        s = e //
+        if(d === "L") {
+            e = s - e
+            interval.push([e,s])
+        }
+        s = e
     }
-    // return arr
+
+    for(let i = 0; i < interval.length; i++ ) {
+        const nums = interval[i]
+        for(let j = 0;  j<2; j++) {
+            arr[nums[j]]++
+        }
+    }
+    return arr
 }
 
 console.log(solution())
