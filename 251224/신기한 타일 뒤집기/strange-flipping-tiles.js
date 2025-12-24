ca32bda6002087f47f6198843dab9d9b7794b51b
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-24 10:26:36 +0900

    Title: 신기한 타일 뒤집기, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251224/신기한 타일 뒤집기/strange-flipping-tiles.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1);
+
+// Please Write your code here.
+
+function solution() {
+
+    let arr = Array(20+1).fill(0)
+    let s = 10
+    let e = 0
+    let cur = 10
+    let r = 0
+    let l = 0
+    
+    const orders = commands.map((arr) => arr.split(" "))
+
+    for(let i = 0; i < orders.length;i++) {
+        const order = orders[i]
+        const step = Number(order[0]) // 5
+        const dir = order[1] //L
+
+        if(dir === "R") {
+            s = cur //10
+            e = cur + step //14
+            cur = e - 1 // 13
+            console.log(s,e,cur)
+
+        } else if( dir === "L") {
+            s = e - step + 1 // 14 - 5 + 1 = 9+1 = 10
+            e = cur + 1 // 14
+            cur = s // 10
+            console.log(s,e)
+        }
+
+        for(let j = s; j < e; j++) {
+            arr[j] = dir // 10 < 14 arr[10<14] = "R"
+        }
+        console.log(arr)
+    }
+
+    for(let i = 0; i< arr.length; i++) {
+        if(arr[i] === "R") {
+            r++
+        }
+        else if(arr[i] === "L") {
+            l++
+        }
+    }
+
+    console.log(l,r)
+
+}
+
+solution()
