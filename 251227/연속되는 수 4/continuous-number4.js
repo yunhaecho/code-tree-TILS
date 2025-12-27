86661f9752b23f9170844dbccb5f7ef0da489e17
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-27 17:57:38 +0900

    Title: 연속되는 수 4, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251227/연속되는 수 4/continuous-number4.js
+++ 251227/연속되는 수 4/continuous-number4.js
@@ -1,30 +1,27 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const n = Number(input[0]);
+const n = Number(input[0])
 const arr = input.slice(1, 1 + n).map(Number);
+
 // Please Write your code here.
-
-function solution () {
-    let result = []
-    let len = []
-
-    if(n === 1) return 1
-
-    for(let i = 0 ; i < arr.length; i++) {
-        if(i === 0 ) {
-            result.push(arr[i])
-            continue
-        }
-
-        if(arr[i] <= arr[i-1]) {
-            len.push(result.length)
-            result = []
-        }
-        result.push(arr[i])
-    }
-    len.push(result.length)
+function solution() {
+    let max = 1
+    let curLen = 0
     
-    return Math.max(...len)
+    if (n === 1) return 1
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i - 1]) {
+            curLen++
+            if(curLen > max) max = curLen
+        } else {
+            curLen = 1
+        }
+    }
+    if(curLen > max) max = curLen
+    
+    return max
+
 }
 
 console.log(solution())
