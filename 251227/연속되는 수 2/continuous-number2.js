aa65e6637e01b2c797e6e7fa0724edbbf4deec75
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-27 16:34:41 +0900

    Title: 연속되는 수 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251227/연속되는 수 2/continuous-number2.js
+++ 251227/연속되는 수 2/continuous-number2.js
@@ -7,6 +7,8 @@ const arr = input.slice(1, n + 1).map(Number);
 // Please Write your code here.
 
 function solution() {
+    if(n === 1) return 1
+
     let cnt = 0                             //[2,7,7,7,7,5,7]
     let cntArr = Array(arr.length).fill(0) //[1,0,0,0,0,0,0]
     for(let i = 0; i< arr.length-1; i++) { //6
@@ -23,6 +25,8 @@ function solution() {
             // console.log(cntArr)
         }
     }
+
+
     return Math.max(...cntArr)
 }
 console.log(solution())
