29877ea640db561fa4057767195309234fe383b6
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-30 19:35:58 +0900

    Title: 연속되는 수 2, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251230/연속되는 수 2/continuous-number2.js
+++ 251230/연속되는 수 2/continuous-number2.js
@@ -7,24 +7,19 @@ const arr = input.slice(1, n + 1).map(Number);
 
 function solution() {
     if(n === 1) return 1
-
     let cnt = 0                             
-    let cntArr = Array(arr.length).fill(0)
-    
-    for(let i = 1; i < arr.length; i++) { 
-        // if(i === 0 ) {
-        //     cnt++
-        //     cntArr[0] = cnt
-        //     continue
-        // }
+    let k = 0
 
-        if(arr[i] != arr[i-1]) cnt = 1
+    for(let i = 0; i < arr.length; i++) { 
+        if(i >= 1 && arr[i] === arr[i-1]){
+            cnt += 1
+        } else {
+            cnt = 1
+        }
 
-        if(i >=1 || arr[i] === arr[i-1]) cnt++
-
-        cntArr[i] = cnt
+        k = Math.max(cnt,k)
     }
 
-    return Math.max(...cntArr)
+    return k
 }
 console.log(solution())
