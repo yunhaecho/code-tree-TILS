f00144e63051524631f5570ec7c1297f69f11fb4
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-30 19:15:36 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251230/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 251230/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -7,56 +7,12 @@ const rect2 = input[1].split(' ').map(Number);
 // Please Write your code here.
 
 function solution() {
-    let arr = Array(2001).fill(0).map((cell) => cell = Array(2001).fill(0))
-    const rects = [rect1, rect2]
-    let xMin = 2000
-    let xMax = 0
-    let yMin = 2000
-    let yMax = 0
-    let sum = 0
+    const x1 = Math.min(rect1[0], rect2[0])
+    const y1 = Math.max(rect1[1], rect2[1])
+    const x2 = Math.min(rect1[2], rect2[2])
+    const y2 = Math.max(rect1[3], rect2[3])
 
-    for (let i = 0; i < rects.length; i++) {
-        const [x1, y1, x2, y2] = rects[i].map((num) => num += 1000)
-        for (let x = x1; x < x2; x++) {
-            for (let y = y1; y < y2; y++) {
-                arr[x][y] = i + 1
-            }
-        }
-    }
-
-    for (let x = 0; x <arr.length; x++) { 
-        const row = arr[x]
-        for (let y = 0; y < row.length; y++) { 
-            if (arr[x][y] === 1) {
-                xMin = Math.min(xMin, x);
-                yMin = Math.min(yMin, y);
-                xMax = Math.max(xMax, x);
-                yMax = Math.max(yMax, y);
-            }
-        }
-    }
-
-    // console.log(xMin,yMin,xMax,yMax)
-
-    if(xMin === 2000 && yMin=== 2000 ) return 0
-
-    // for (let x = 0; x < arr.length; x++) {
-    //     const row = arr[x]
-    //     for (let y = 0; y < row.length; y++) {
-    //         if (arr[x][y] === 1) {
-    //             if (x > xMax) xMax = x
-    //             if (y > yMax) yMax = y
-    //         }
-    //     }
-    // }
-
-    // for (let x = xMin; x <= xMax; x++) {
-    //     for (let y = yMin; y <= yMax; y++) {
-    //         sum++
-    //     }
-    // }
-
-    return (xMax - xMin + 1) * (yMax - yMin + 1) 
+    return (x2 - x1) * (y2 - y1) 
 }
 // solution()
 console.log(solution())
