6847ef0b7aba72fc8846487b29bbb2be04004c21
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2025-12-30 18:25:24 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 168ms, Memory: 59MB, Status: Wrong Answer - Codetree

--- 251230/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 251230/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -7,7 +7,7 @@ const rect2 = input[1].split(' ').map(Number);
 // Please Write your code here.
 
 function solution() {
-    let arr = Array(21).fill(0).map((cell) => cell = Array(21).fill(0))
+    let arr = Array(2001).fill(0).map((cell) => cell = Array(2001).fill(0))
     const rects = [rect1, rect2]
     let xMin = 0
     let xMax = 0
@@ -16,7 +16,7 @@ function solution() {
     let sum = 0
 
     for (let i = 0; i < rects.length; i++) {
-        const [x1, y1, x2, y2] = rects[i].map((num) => num += 10)
+        const [x1, y1, x2, y2] = rects[i].map((num) => num += 1000)
         for (let x = x1; x < x2; x++) {
             for (let y = y1; y < y2; y++) {
                 arr[x][y] = i + 1
@@ -24,39 +24,35 @@ function solution() {
         }
     }
 
-                    console.log(arr)
+    minLoop : for (let x = 0; x < arr.length; x++) {
+        const row = arr[x]
+        for (let y = 0; y < row.length; y++) {
+            if (arr[x][y] === 1) {
+                xMin = x
+                yMin = y
+                break minLoop;
+            }
+        }
+    }
 
+    for (let x = 0; x < arr.length; x++) {
+        const row = arr[x]
+        for (let y = 0; y < row.length; y++) {
+            if (arr[x][y] === 1) {
+                if (x > xMax) xMax = x
+                if (y > yMax) yMax = y
+            }
+        }
+    }
 
+    for (let x = xMin; x <= xMax; x++) {
+        for (let y = yMin; y <= yMax; y++) {
+            sum++
+        }
+    }
 
-    // for (let x = 0; x < arr.length; x++) {
-    //     const row = arr[x]
-    //     for (let y = 0; y < row.length; y++) {
-    //         if (arr[x][y] === 1) {
-    //             xMin = x
-    //             yMin = y
-    //             break
-    //         }
-    //     }
-    // }
-
-    // for (let x = 0; x < arr.length; x++) {
-    //     const row = arr[x]
-    //     for (let y = 0; y < row.length; y++) {
-    //         if (arr[x][y] === 1) {
-    //             if (x > xMax) xMax = x
-    //             if (y > yMax) yMax = y
-    //         }
-    //     }
-    // }
-
-    // for (let x = xMin; x < xMax; x++) {
-    //     for (let y = yMin; y < yMax; y++) {
-    //         sum++
-    //     }
-    // }
-
-    // return sum
+    return sum
 
 }
-
+// solution()
 console.log(solution())
