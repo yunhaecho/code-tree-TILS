dc34462045c80d0896215942adc9cc5d6b462a5d
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-02 22:17:39 +0900

    Title: 선두를 지켜라, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260102/선두를 지켜라/keep-the-lead.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const aData = input.slice(1, n + 1).map(line => line.split(" ").map(Number));
+const bData = input.slice(n + 1, n + 1 + m).map(line => line.split(" ").map(Number));
+
+// Please Write your code here.
+
+function recordLocation (data) {
+    const distance = []
+    let location = 0
+
+    for(let i = 0; i < data.length; i++) {
+        const [v,t] = data[i]
+        for(let j = 0; j < t; j++) {
+            location += v
+            distance.push(location)
+        }
+    }
+
+    return distance
+}
+
+function solution() {
+    let cnt = -1
+    const positionA = recordLocation(aData)
+    const positionB = recordLocation(bData)
+
+    let status = "start"
+
+    for(let i = 0; i < positionA.length; i++) {
+        console.log(i, status, cnt)
+        if(positionA[i] > positionB[i]) {
+            if(status === "a") continue
+            status = "a"
+            cnt++
+
+        } else if(positionA[i] < positionB[i]) {
+            if(status === "b") continue
+            status = "b"
+            cnt++
+        } 
+        
+    }
+
+    return cnt
+}
+
+console.log(solution())
