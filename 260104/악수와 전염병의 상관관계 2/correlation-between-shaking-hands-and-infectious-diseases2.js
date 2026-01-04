f90ff3f24badce439d5a568be00e97ba29c3ebf5
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-04 17:24:56 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260104/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
+++ 260104/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
@@ -4,32 +4,32 @@ const [n, k, p, t] = input[0].split(' ').map(Number);
 const shakes = [];
 for (let i = 1; i <= t; i++) {
     const [time, person1, person2] = input[i].split(' ').map(Number);
-    shakes.push([time, person1, person2 ]);
+    shakes.push([time, person1, person2]);
 }
 
 // Please write your code here.
 function solution() {
     const result = Array(n).fill(0)
-    result[p-1] = 1
+    const shakesCnt = Array(n).fill(k)
+
+    result[p - 1] = 1
     shakes.sort()
-    let cnt = k
 
-    for(let i = 0; i < shakes.length; i++) {
-        const [time, p1, p2]  = shakes[i]
+    for (let i = 0; i < shakes.length; i++) {
+        const [time, p1, p2] = shakes[i]
 
-        if(result[p1-1] === 1 ) {
-            result[p2-1] = 1
-            cnt -= 1
-        } else if(result[p2-1] === 1){
-            result[p1-1] = 1
-            cnt -= 1
-        }
-        
-         if(cnt === 0 ){
-            return result.join("")
+        if (shakesCnt[p1 - 1] > 0 && shakesCnt[p2 - 1] > 0) {
+            if (result[p1 - 1] === 1) {
+                result[p2 - 1] = 1
+                shakesCnt[p1 - 1] -= 1
+            } else if (result[p2 - 1] === 1) {
+                result[p1 - 1] = 1
+                shakesCnt[p2 - 1] -= 1
+            }
         }
 
     }
+    return result.join("")
 
 }
 
