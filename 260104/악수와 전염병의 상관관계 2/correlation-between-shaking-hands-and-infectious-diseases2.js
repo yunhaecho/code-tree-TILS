081b8a32e4588844c43b0e17203028c65b5d729d
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-04 16:55:30 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260104/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k, p, t] = input[0].split(' ').map(Number);
+const shakes = [];
+for (let i = 1; i <= t; i++) {
+    const [time, person1, person2] = input[i].split(' ').map(Number);
+    shakes.push({ time, person1, person2 });
+}
+// Please write your code here.
+function solution() {
+    console.log(shakes.sort())
+}
+
+solution()
+
