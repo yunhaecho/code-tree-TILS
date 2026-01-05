1f1ddec8bd16c07c979fa1eed34adb954806de66
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-05 21:50:41 +0900

    Title: 방향에 맞춰 이동, Time: 71ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260105/방향에 맞춰 이동/move-in-direction.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const moves = input.slice(1);
+
+// Please Write your code here.
+
+function solution () {
+    console.log(moves)
+}
+
+solution()
