67ab275ec29bc9280b78b6bdbc0845a98be3367b
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-11 23:50:12 +0900

    Title: 1이 3개 이상 있는 위치, Time: 75ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260111/1이 3개 이상 있는 위치/place-more-than-3-ones.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const grid = input.slice(1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+const inRange = (x, y) => {
+    return x >= 0 && x < n && y >= 0 && y < n
+}
+
+function solution() {
+    const dx = [-1, 1, 0, 0]
+    const dy = [0, 0, -1, 1]
+
+    let cntOne = 0
+    let cntThree = 0
+
+    let x = 0
+    let y = 0
+
+    for (let i = 0; i < n; i++) { //한 행만큼 = n
+        for (let k = 0; k < n; k++) { //한 열에 대해서 
+            for (let j = 0; j < dx.length; j++) { //4가지 방향에 대해서 
+                x = x + dx[j]
+                y = y + dy[j]
+
+                if (inRange(x, y)) {
+                    if (grid[x][y] === 1) cntOne +=1
+                }
+                x = i
+                y = k
+            }
+            if (cntOne >= 3) cntThree++
+            cntOne = 0
+        }
+    }
+
+
+    return cntThree
+
+}
+
+console.log(solution())
