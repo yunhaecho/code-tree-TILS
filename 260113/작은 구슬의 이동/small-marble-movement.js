c234ae831f9ebf3f2827664b8d30270622615384
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-13 15:03:28 +0900

    Title: 작은 구슬의 이동, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260113/작은 구슬의 이동/small-marble-movement.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, t] = input[0].split(" ").map(Number);
+let [r, c, d] = input[1].split(" ");
+r = Number(r);
+c = Number(c);
+
+// Please Write your code here.
+const inRange = (x,y,n) => {
+    return 1 <= x && x <= n && 1 <= y && y <= n
+}
+
+function solution() {
+    const dx = [-1,0,0,1]
+    const dy = [0, -1,1,0]
+    let nx = r
+    let ny = c
+    
+    const dirChar = {}
+    dirChar['U'] = 0
+    dirChar['D'] = 3
+    dirChar['L'] = 1
+    dirChar['R'] = 2
+    
+    let dirNum = dirChar[d]
+
+    //4초동안 반복하면서
+    for (let i = 0; i < t; i++) {
+
+        if (!inRange(nx +dx[dirNum],ny+dy[dirNum],n)) {
+            dirNum = 3 - dirChar[d]
+            continue
+        }
+            nx += dx[dirNum]
+            ny += dy[dirNum]
+}
+    return [nx,ny]
+
+}
+console.log(solution().join(" "))
