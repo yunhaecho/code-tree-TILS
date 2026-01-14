fe9740b5e99dae82778407e0c0184585c1d71747
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-14 18:24:29 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 79ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 260114/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+// Please Write your code here.
+const eg = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const inRange = (x,y) => {
+    return 0 <= x && x < n && 0 <= y && y < m
+}
+
+function solution() {
+
+    const arr = Array(n).fill(0).map(() => Array(m).fill(0))
+    let dirNum = 0
+
+    const dx = [0,1,0,-1]
+    const dy = [1,0,-1,0]
+    let x = 0
+    let y = 0
+    let num = 0
+
+    while(num < n*m) {
+
+        if(arr[x][y] === 0 ){
+            arr[x][y] = eg[num]
+        }
+        num++
+
+        let nx = x + dx[dirNum]
+        let ny = y + dy[dirNum]
+
+        if(!inRange(nx,ny)||arr[nx][ny] !== 0 ) {
+            dirNum = (dirNum + 1) % 4
+            nx = x + dx[dirNum]
+            ny = y + dy[dirNum]
+        }
+
+        x = nx
+        y = ny
+
+    }
+
+    arr.map((line) => console.log(line.join(" ")))
+}
+solution()
