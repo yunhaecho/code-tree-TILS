c384512f5323e6516437ec82959a60f81ca404ce
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-14 18:19:18 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260114/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+const inRange = (x,y) => {
+    return 0 <= x && x < n && 0 <= y && y < m
+}
+
+function solution() {
+
+    const arr = Array(n).fill(0).map(() => Array(m).fill(0))
+    let dirNum = 0
+
+    const dx = [1,0,-1,0]
+    const dy = [0,1,0,-1]
+    let x = 0
+    let y = 0
+    let num = 0
+
+    while(num < n*m) {
+
+        if(arr[x][y] === 0 ){
+            num+=1
+            arr[x][y] = num
+        }
+
+        let nx = x + dx[dirNum]
+        let ny = y + dy[dirNum]
+
+        if(!inRange(nx,ny)||arr[nx][ny] !== 0 ) {
+            dirNum = (dirNum + 1) % 4
+            nx = x + dx[dirNum]
+            ny = y + dy[dirNum]
+        }
+
+        x = nx
+        y = ny
+
+    }
+
+    arr.map((line) => console.log(line.join(" ")))
+}
+
+solution()
+
