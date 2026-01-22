b48fb778672044cc0eb51ba4bbeb21b2f39cdb1f
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-22 19:48:05 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260122/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = Array.from({length: n}, (_, i) => input[i + 1].split(' ').map(Number));
+
+// Please Write your code here.
+
+const calculate = (x1,y1,x2,y2) => {
+    return Math.abs((x1-x2)) + Math.abs((y1 - y2)) 
+}
+
+function solution() {
+    let min = Number.MAX_SAFE_INTEGER;
+    
+    for(let i = 1; i < n-1; i++) {
+        let jumpIdx = i
+        let cx = 0
+        let cy = 0
+        let sum = 0
+        for(let j = 1; j < n; j++) {
+            if(jumpIdx === j) continue
+            let [nx, ny] = arr[j]
+            sum += calculate(cx,cy,nx,ny)
+            cx = nx
+            cy = ny
+        }
+        min = Math.min(sum,min)
+    }
+
+    return min
+
+}
+
+console.log(solution())
