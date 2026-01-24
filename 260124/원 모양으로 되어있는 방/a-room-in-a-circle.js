bda7affa6d2df930fe66744079ee4df872f0aa60
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-24 14:58:11 +0900

    Title: 원 모양으로 되어있는 방, Time: 74ms, Memory: 10MB, Status: Passed - Codetree

--- 260124/원 모양으로 되어있는 방/a-room-in-a-circle.js
+++ 260124/원 모양으로 되어있는 방/a-room-in-a-circle.js
@@ -4,10 +4,10 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
-// Please Write your code here.
+// Please Write your code here
 function solution () {
     let sum = 0
-    let min = 1000
+    let min = Number.MAX_SAFE_INTEGER
     for(let i = -2; i < n; i++) {
         sum = 0
         let startRoom = (i + 1) % n
