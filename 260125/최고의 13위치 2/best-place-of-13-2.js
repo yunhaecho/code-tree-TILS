2d5e2d4a40d79a73bb256b0410994a211d18c420
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-25 23:27:16 +0900

    Title: 최고의 13위치 2, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260125/최고의 13위치 2/best-place-of-13-2.js
+++ 260125/최고의 13위치 2/best-place-of-13-2.js
@@ -10,14 +10,12 @@ function solution() {
     let max = Number.MIN_SAFE_INTEGER
 
     for (let i = 0; i < n - 1 ; i++) {
-        for (let j = 0; j < n - 3; j++) {
+        for (let j = 0; j <= n - 3; j++) {
             for (let x = i + 1; x < n; x++) {
-                for (let y = 0; y < n - 3; y++) {
+                for (let y = 0; y <= n - 3; y++) {
                     const box1 = grid[i][j] + grid[i][j+1] + grid[i][j+2]
                     const box2 = grid[x][y] + grid[x][y+1] + grid[x][y+2]
-                    console.log(box1, box2)
-                    const sum = box1 + box2
-                    max = Math.max(max, sum)
+                    max = Math.max(max, box1 + box2)
                 }
             }
         }
