b5e68cbefeb7c68514aa2e7a8e28084585c7d176
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-25 22:49:32 +0900

    Title: Carry 피하기 2, Time: 61ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260125/Carry 피하기 2/escaping-carry-2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n).map(Number);
+
+// Please Write your code here.
+
+function solution() {
+    
+}
