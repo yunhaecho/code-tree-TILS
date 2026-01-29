3b6d5a47803fc0c904ba35199c328b941a81a39d
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-01-29 18:21:59 +0900

    Title: G or H 3, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260129/G or H 3/G-or-H-3.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const people = [];
+for (let i = 1; i <= n; i++) {
+  const [x, c] = input[i].split(' ');
+  people.push([Number(x), c]);
+}
+
+// Please Write your code here.
+function solution () {
+  const arr = Array(20).fill(0)
+  for(let i = 0; i < n; i++) {
+    const [x,c] = people[i]
+    arr[x] = c
+  }
+
+  let max = Number.MIN_SAFE_INTEGER
+  for(let i = 0; i < n-k; i++) {
+    let sum = 0
+    console.log(`i = ${i}`)
+    for(let j = i; j < i+k+1; j++) {
+        if(arr[j] === "G") {
+          sum += 1
+        } else {
+          sum+=2
+        }
+    }
+    max = Math.max(sum,max)
+  }
+
+  return max
+}
+
+console.log(solution())
