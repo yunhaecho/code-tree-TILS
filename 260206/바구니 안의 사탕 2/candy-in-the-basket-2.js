407d105bc976ed4e9a9631d487a3f8f688f8553e
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-02-06 13:28:53 +0900

    Title: 바구니 안의 사탕 2, Time: 65ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260206/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+let index = 0;
+const [n, k] = input[index++].split(" ").map(Number);
+
+const baskets = [];
+for (let i = 0; i < n; i++) {
+    const [candy, position] = input[index++].split(" ").map(Number);
+    baskets.push([candy, position]);
+}
+
+// Please Write your code here.
+const candies = Array(20).fill(0)
+let max = Number.MIN_SAFE_INTEGER
+
+for(const [candy,position] of baskets) {
+    candies[position] = candy
+}
+
+// for(let i = 0 ; i< candies.length; i++) {
+    for(let c = k; c <= 20-k; c++) {
+        let sum = 0
+        console.log(candies.split(c-k,c+k))
+        // for(let z = c-k; z< c+k; z++) {
+        //     sum += candies[z]
+        // }
+        // max = Math.max(sum,max)
+    }
+// }
+// console.log(max)
