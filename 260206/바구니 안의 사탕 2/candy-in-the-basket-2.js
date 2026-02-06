9318ff5ce687b4d38201b1e7d962f8144be699b4
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-02-06 13:41:29 +0900

    Title: 바구니 안의 사탕 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260206/바구니 안의 사탕 2/candy-in-the-basket-2.js
+++ 260206/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -11,17 +11,19 @@ for (let i = 0; i < n; i++) {
 }
 
 // Please Write your code here.
-const candies = Array(20).fill(0)//401
-let max = Number.MIN_SAFE_INTEGER
+const candies = Array(401).fill(0)//401
+let max = -1
+let maxPosition = -1
 
 for(const [candy,position] of baskets) {
+    maxPosition = Math.max(position,maxPosition)
     candies[position] = candy
 }
 
 // for(let i = 0 ; i< candies.length; i++) {
-    for(let c = k; c <= 15-k; c++) {
+    for(let c = k; c <= maxPosition-k; c++) {
         let sum = 0
-        for(let z = c-k; z<= c+k; z++) {
+        for(let z = c-k; z <= c+k; z++) {
             sum += candies[z]
         }
         max = Math.max(sum,max)
