750bac0e0f8b7604ce0ebe36275b63c476e3d448
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-03-02 18:14:21 +0900

    Title: 행복한 수열의 개수, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260302/행복한 수열의 개수/number-of-happy-sequence.js
+++ 260302/행복한 수열의 개수/number-of-happy-sequence.js
@@ -6,57 +6,42 @@ const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 
-const row = () => {
-        let cnt = 0
+
+const checkHappy = (arr) => {
+    let cnt =0 
     let same = 0
-    for (let row = 0; row < n; row++) {
-        same = 0
-        for (let idx = 0; idx < n - 1; idx++) {
-            if (grid[row][idx] === grid[row][idx + 1]) {
-                same++
-                if (same >= m-1) {
+    for(let i = 0; i < arr.length-1; i++) {
+        if(arr[i] === arr[i+1]) {
+            same++
+            if (same >= m-1) {
                     cnt++
                     break
                 }
-            } else {
-                same=0
-            }
+        } else {
+            same = 0
         }
     }
     return cnt
 }
 
-const col = () => {
-    let cnt = 0
-    let same = 0
+function solution () {
+    let result = 0
     
-    for (let col = 0; col < n; col++) {
-        same = 0
-        for (let idx = 0; idx < n - 1; idx++) {
-            if (grid[idx][col] === grid[idx + 1][col]) {
-                same++
-                if (same >= m-1) {
-                    cnt++
-                    break
-                }
-            } else {
-                same=0
-            }
+    for(let row = 0; row < n; row++) {
+        const rowArr = grid[row]
+        result += checkHappy(rowArr);
+    }
+
+    for(let col = 0; col < n; col++) {
+        let colArr = []
+        for (let idx = 0; idx < n; idx++) {
+            colArr.push(grid[idx][col])
         }
+        result += checkHappy(colArr)
     }
-    return cnt
 
+    return result
 }
 
-function checkHappy (dir) {
-    if(dir === "row") {
-        return row()
-    } else {
-        return col()
-    }
-}
 
-const searchRow = checkHappy("row");
-const searchCol = checkHappy("col");
-
-console.log(m === 1 ? n*2: searchRow + searchCol)
+console.log(m === 1 ? n*2: solution())
