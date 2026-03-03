d5c0d8eaf77d702a040d7dc7adbfccc47ffe009d
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-03-03 20:37:47 +0900

    Title: 컨베이어 벨트, Time: 72ms, Memory: 10MB, Status: Passed - Codetree

--- 260303/컨베이어 벨트/conveyor-belt.js
+++ 260303/컨베이어 벨트/conveyor-belt.js
@@ -5,19 +5,18 @@ const u = input[1].split(' ').map(Number);
 const d = input[2].split(' ').map(Number);
 
 // Please write your code here.
-const d_reverse= d.reverse()
-for(let i = 0; i < t; i++) {
-    const back = u[n-1]
-    const front = d_reverse[0]
-    for(let j = n-1; j >0; j--) {
-        u[j] = u[j-1]
+for(let _ = 0; _ < t; _++) {
+    const carry = u[n-1]
+    for(let u_Idx = n-1; u_Idx >0; u_Idx--) {
+        u[u_Idx] = u[u_Idx-1];
     }
-    for(let k = 1; k<n; k++) {
-        d_reverse[k-1] = d_reverse[k]
+    u[0] = d[n-1];
+
+    for(let d_Idx = n-1; d_Idx >0; d_Idx--) {
+        d[d_Idx] = d[d_Idx-1];
     }
-    u[0] = front;
-    d_reverse[n-1] = back;
+    d[0] = carry;
 }
 
 console.log(u.join(" "))
-console.log(d_reverse.reverse().join(" "))
+console.log(d.join(" "))
