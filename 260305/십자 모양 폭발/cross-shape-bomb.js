e2688f06e77a66f61135054ce26050864a573561
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-03-05 22:43:20 +0900

    Title: 십자 모양 폭발, Time: 74ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260305/십자 모양 폭발/cross-shape-bomb.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+const [r, c] = input[1 + n].split(' ').map(Number);
+
+// Please Write your code here.
+const bombSize = grid[r - 1][c - 1]
+grid[r - 1][c - 1] = 0
+
+const dropNumber = (grid) => {
+    let arr = []
+    const minCol = c - 1 - (bombSize - 1) < 0 ? 0 : c - 1 - (bombSize - 1)
+    for (let i = minCol; i < n; i++) {
+        for (let j = n - 1; j >= 0; j--) {
+            arr.push(grid[j][i])
+        }
+
+        for (let j = 0; j < n; j++) {
+            if (arr[j] === 0) {
+                arr = arr.filter((num)=> num != arr[j])
+            }
+        }
+
+        for(let j = n-1; j >= 0; j--){
+            let num = arr.shift()
+            num = num === undefined ? 0 : num
+            grid[j][i] = num
+        }
+
+    }
+    return grid
+}
+
+
+
+if (bombSize === 1) return dropNumber(grid);
+
+const dirX = [0, -1, 0, 1]//좌상우하
+const dirY = [-1, 0, 1, 0]
+//0으로 바꾸기 작업
+let x = r - 1
+let y = c - 1
+
+for (let dir = 0; dir < 4; dir++) {
+    for (let _ = 0; _ < bombSize - 1; _++) {
+
+        let curX = x - dirX[dir]
+        let curY = y - dirY[dir]
+
+        if (0 <= curX && curX < n && 0 <= curY && curY < n) {
+            grid[curX][curY] = 0
+        }
+        x = curX
+        y = curY
+    }
+    x = r - 1
+    y = c - 1
+
+}
+
+const result = dropNumber(grid);
+
+for(let row of result) {
+    console.log(row.join(" "))
+}
+
