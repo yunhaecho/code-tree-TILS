c6913c13c24d36071b0a050262daa0d158c3ad85
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-03-05 23:32:10 +0900

    Title: 십자 모양 폭발, Time: 90ms, Memory: 13MB, Status: Passed - Codetree

--- 260305/십자 모양 폭발/cross-shape-bomb.js
+++ 260305/십자 모양 폭발/cross-shape-bomb.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+const grid = input.slice(1, 1 + n).map(line => line.trim().split(/\s+/).map(Number));
 const [r, c] = input[1 + n].split(' ').map(Number);
 
 // Please Write your code here.
@@ -10,24 +10,20 @@ const bombSize = grid[r - 1][c - 1]
 grid[r - 1][c - 1] = 0
 
 const dropNumber = (grid) => {
-    const minCol = c - 1 - (bombSize - 1) < 0 ? 0 : c - 1 - (bombSize - 1)
-    const maxCol = c - 1 + (bombSize - 1) >= n ? n - 1 : c - 1 + (bombSize - 1)
-
-    for (let i = minCol; i <= maxCol; i++) {
+    for (let i = 0; i < n; i++) {
         let arr = []
         for (let j = 0; j < n; j++) {
             arr.push(grid[j][i])
         }
 
         arr = arr.filter((num) => num != 0)
-        const arrLen = arr.length
-        for (let j = 0; j < n - arrLen; j++) {
+
+        while( arr.length < n){
             arr.unshift(0)
         }
 
         for (let j = n - 1; j >= 0; j--) {
-            let num = arr.pop()
-            grid[j][i] = num
+            grid[j][i] = arr.pop()
         }
     }
     return grid
@@ -36,30 +32,22 @@ const dropNumber = (grid) => {
 const dirX = [0, -1, 0, 1]//좌상우하
 const dirY = [-1, 0, 1, 0]
 //0으로 바꾸기 작업
-let x = r - 1
-let y = c - 1
 
 for (let dir = 0; dir < 4; dir++) {
-    for (let _ = 0; _ < bombSize - 1; _++) {
+    for (let dist = 1; dist < bombSize; dist++) {
 
-        let curX = x + dirX[dir]
-        let curY = y + dirY[dir]
+        let curX = (r - 1) + dirX[dir] * dist
+        let curY = (c - 1) + dirY[dir] * dist
 
         if (0 <= curX && curX < n && 0 <= curY && curY < n) {
             grid[curX][curY] = 0
         }
-        x = curX
-        y = curY
     }
-    x = r - 1
-    y = c - 1
-
 }
 
 const result = dropNumber(grid);
 
 for (let row of result) {
-
     console.log(row.join(" "))
 }
 
