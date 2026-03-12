41a97e8c9b05b1a9005d298a9d4e6c6a1410193c
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-03-12 15:33:49 +0900

    Title: 떨어지는 1자 블록, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260312/떨어지는 1자 블록/falling-horizontal-block.js
+++ 260312/떨어지는 1자 블록/falling-horizontal-block.js
@@ -2,19 +2,17 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m, k] = input[0].split(' ').map(Number);
-const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
-
+const grid = input.slice(1, 1 + n).map(line => 
+    line.trim().split(/\s+/).map(Number)
+);
 // Please Write your code here.
 
 function solution() {
-    // if (n === 1) {
-    //     console.log(1)
-    //     return;
-    // }
+
     let targetRow = 0;
-    let canGo = true;
 
     for (let i = 0; i < n; i++) {
+        let canGo = true;
         for (let j = k - 1; j < k + m - 1; j++) {
             if (grid[i][j]) {
                 canGo = false;
