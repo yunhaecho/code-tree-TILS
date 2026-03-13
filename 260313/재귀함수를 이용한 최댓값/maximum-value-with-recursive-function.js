68c2926af968558764617da05afdff5b6720f74f
Yunhae Cho <91577577+yunhaecho@users.noreply.github.com>
2026-03-13 23:05:57 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260313/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 260313/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -4,16 +4,15 @@ const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
 
 // Please Write your code here.
-// [1 5 7 9 2 6]
+// // [1 5 7 9 2 6]
 
-let max = 0
-let idx = n - 1 
+// let max = 0
+// let idx = n - 1 
 
-function solution(idx) {
-    if(idx === -1) return;
-    solution(idx-1)
-    max = Math.max(max,arr[idx]);
-    return max
+function maxValue(a) {
+    if (a === 0) return arr[0];
+
+    return Math.max(maxValue(a - 1), arr[a]);
 }
 
-console.log(solution(idx))
+console.log(maxValue(n - 1));
